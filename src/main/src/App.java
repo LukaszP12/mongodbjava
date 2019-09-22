7de49ad79e48ab56126ab15f62aa3327966ef486
LukaszP <lukaszp4@onet.eu>
2019-09-22 18:05:20 +0200

    3rd commit

--- src/main/src/App.java
+++ src/main/src/App.java
@@ -1,12 +1,19 @@
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.ClientSessionOptions;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.client.*;
 import org.bson.Document; // bson to biblioteka MongoDB
+import org.bson.codecs.DocumentCodec;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.bson.conversions.Bson;
 
+import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class App {
 
@@ -14,14 +21,53 @@ public class App {
 
        //  MongoClient mongoClient = new MongoClient("localhost", 27017); //  27017 to domyślny port dla każdego serwera MongoDB
 
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
+       //  MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+        CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+        MongoClient mongoClient = new MongoClient("localhost", MongoClientOptions.builder().codecRegistry(pojoCodecRegistry).build());
+
         MongoDatabase mongoDatabase = mongoClient.getDatabase("Toys");
         MongoCollection mongoCollection = mongoDatabase.getCollection("Doll");
 
         // Save information to MongoDB
         save(mongoCollection);
         read(mongoCollection);
+        readByParam(mongoCollection, "Mark","BMW");
+        delete(mongoCollection,"Mark","BMW");
 
+    }
+
+    private static void update(MongoCollection mongoCollection){
+
+        Document document = new Document();
+        document.put("Mark","Audi");
+        Document documentFound = (Document) mongoCollection.find(document).first();
+
+        Document documentUpdated = new Document();
+        documentUpdated.put("Audi", "A2");
+        documentUpdated.put("Color","Black");
+        mongoCollection.updateOne(document,documentUpdated);
+    }
+
+    private static void delete(MongoCollection mongoCollection, String param, Object value) {
+        Document document = new Document();
+        document.put(param,value);
+        mongoCollection.deleteOne(document);
+    }
+
+    private static void readByParam(MongoCollection mongoCollection, String param, Object value){
+        Document document = new Document();
+        document.put(param,value);
+        MongoCursor iterator = mongoCollection.find(document).iterator();
+        while (iterator.hasNext())
+        {
+            Document next = (Document) iterator.next();
+            System.out.println(next.toJson());
+        }
+
+        //Document first = (Document) mongoCollection.find(document).first();
+        System.out.println(first.toJson());
     }
 
     private static void read(MongoCollection mongoCollection){
